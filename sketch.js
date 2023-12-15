ad922f3f9cb83fc11e08c05711ca846c1cce18be
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2023-12-15 08:34:03 +0000

    Stuff

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 class Player {
-    constructor(name, score, slotMachines, money) {
+    constructor(name, slotMachines, money) {
         this.name = name;
-        this.score = score;
         this.slotMachines = slotMachines || 1;
         this.money = money || 1000;
     }
 }
 
+const player = new Player("Player 1");
+let slotMachines = [];
 
 class GameState {
     constructor() {
@@ -16,6 +17,7 @@ class GameState {
     }
 
     setup() {
+
         createCanvas(windowWidth, windowHeight);
 
         // Set the background color
@@ -64,12 +66,17 @@ class GameState {
         // Draw the casino
         this.drawCasino();
 
-        // Add code to draw the game
     }
 
     drawCasino() {
+
         // Set the background color
         background(220);
+
+        frameRate(10);
+
+        // Increase the player's money
+        this.increaseMoney();
 
         // Draw the red carpet down the middle
         fill(255, 0, 0); // Red color
@@ -98,12 +105,9 @@ class GameState {
         // Draw the slot machines on the right side accounting for the carpet and the height of the window
         for (let i = 0; i < rightSlotMachineCountPlaceholder; i++) {
             for (let j = 0; j < topSlotMachineCount; j++) {
-                this.drawSlotMachinePlaceholder(width - (i + 1) * slotMachineWidth, j * slotMachineHeight);
+                this.drawSlotMachinePlaceholder(width - (i + 1) * slotMachineWidth, j * slotMachineHeight, "right");
             }
         }
-
-        // Draw the as many slot machines as the player owns ontop of the placeholders accounting for the carpet and the height of the window
-        const player = new Player("Player 1", 1000, 4);
 
         // Calculate how many slot machines can fit on the left and right side of the carpet by checking how many the player owns
         const leftSlotMachineCount = Math.min(Math.floor((width / 2 - carpetWidth / 2) / slotMachineWidth), player.slotMachines);
@@ -129,39 +133,110 @@ class GameState {
             }
         }
 
-        // Check if there are enough spaces for the player's slot machines to be drawn
-        if (totalSlotMachinesDrawn < slotMachineCount) {
-            console.log("Error: Not enough spaces to draw all the player's slot machines");
-
-            // Draw a big red X on the screen
-            fill(255, 255, 0); // Red color
-            textSize(100);
-            textAlign(CENTER);
-            text("X", width / 2, height / 2);
-
-
+        // Draw the slot machines on the right side accounting for the carpet and the height of the window aswell as the number of slot machines the player owns
+        for (let i = 0; i < rightSlotMachineCount; i++) {
+            for (let j = 0; j < topSlotMachineCount; j++) {
+                if (totalSlotMachinesDrawn >= slotMachineCount) {
+                    break;
+                }
+                this.drawSlotMachine(width - (i + 1) * slotMachineWidth, j * slotMachineHeight, "right");
+                totalSlotMachinesDrawn++;
+            }
+            if (totalSlotMachinesDrawn >= slotMachineCount) {
+                break;
+            }
         }
 
+        // Check if there are enough spaces for the player's slot machines to be drawn
+        if (totalSlotMachinesDrawn < slotMachineCount) {
+
+            // Write a small message in the bottom right of the screen saying how many hidden slot machines there are
+            fill(0); // Grey
+            textSize(20);
+            textAlign(RIGHT);
+            text(`${slotMachineCount - totalSlotMachinesDrawn} hidden slot machines`, width - 10, height - 30);
+        }
+
+        // Draw the player's money
+        fill(0);
+        textSize(20);
+        textAlign(LEFT);
+        text(`Money: ${player.money}`, 10, height - 30);
+
+        //Calculate the cost of the nest slot machine
+        const nextSlotMachineCost = 500 * player.slotMachines;
+
+        // Setup the buy slot machine button
+        const buySlotMachineButton = createButton("Buy Slot Machine ($" + nextSlotMachineCost + ")");
+        buySlotMachineButton.position(10, height - 100);
+        buySlotMachineButton.size(200, 50);
+        buySlotMachineButton.style("background-color", "white");
+
+        // Add a click event listener to the button
+        buySlotMachineButton.mousePressed(() => {
+            console.log("Clicked on buy slot machine button");
+            this.buySlotMachine(nextSlotMachineCost);
+        });
+    }
+
+    increaseMoney() {
+        // Increase the player's money compared to the number of slot machines they own
+        player.money += player.slotMachines * 2;
+    }
+
+    // Function to buy a slot machine
+    buySlotMachine(price) {
+        // Check if the player has enough money to buy a slot machine
+        if (player.money < price) {
+            console.log("Not enough money to buy a slot machine");
+            alert("Not enough money to buy a slot machine");
+            return;
+        }
+
+        // Add a slot machine to the player's slot machines
+        player.slotMachines++;
+
+        // Subtract 100 from the player's money
+        player.money -= price;
     }
 
     // Function to draw a slot machine
-    drawSlotMachine(x, y) {
+    drawSlotMachine(x, y, margin = "left", number) {
         // Draw the slot machine body
         fill(200); // Gray color
-        rect(x, y, 100, 150);
+        if (margin === "left") {
+            rect(x + 30, y, 100, 150);
 
-        // Draw the screen
-        fill(0); // Black color
-        rect(x + 10, y + 10, 80, 70); 
+            slotMachines.push({ x: x + 30, y: y, width: 100, height: 150, number: number })
+            console.log(slotMachines)
+
+            
+        } else {
+            rect(x, y, 100, 150);
+        }
+
+        if (margin === "left") {
+            // Draw the screen
+            fill(0); // Black color
+            rect(x + 40, y + 10, 80, 70);
+        } else {
+            // Draw the screen
+            fill(0); // Black color
+            rect(x + 10, y + 10, 80, 70);
+        }
     }
 
-    // This is used for indicating where a slot machine can be placed, it's not a real slot machine and has a cost that is shown on the body, make it a rectangle with a text inside
-    drawSlotMachinePlaceholder(x, y, cost = 100) {
+    // This is used for indicating where a slot machine can be placed
+    drawSlotMachinePlaceholder(x, y, margin = "left") {
         // Draw the slot machine body
         fill(200); // Gray color
-        rect(x, y, 100, 150);
+        if (margin === "left") {
+            rect(x + 30, y, 100, 150);
+        } else {
+            rect(x, y, 100, 150);
+        }
     }
-    
+
     windowResized() {
         resizeCanvas(windowWidth, windowHeight);
 
@@ -247,7 +322,7 @@ class GameState {
             console.log("Clicked on the back button");
             this.state = "menu";
         });
-        
+
 
         drawLeaderboard();
     }
